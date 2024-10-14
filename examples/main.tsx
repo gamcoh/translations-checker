5e2ad3db30c72a02f41e47a3fefb37ffdc4214b9
Gamliel Cohen <gamliel.cohen-ext@pernod-ricard.com>
2024-10-14 23:32:21 +0200

    Done the main work, now the executable can check for translations in standalone way.

---
+++ examples/main.tsx
@@ -0,0 +1,47 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import React from "react";
+import Breadcrumb from "src/layouts/full/shared/breadcrumb/Breadcrumb";
+import PageContainer from "src/components/container/PageContainer";
+import EventListing from "src/components/apps/events/EventListing";
+import { useTranslation } from "react-i18next";
+import { useSelector } from "src/store/Store";
+import { Box, Button, Grid, Typography } from "@mui/material";
+
+const Blog = () => {
+  const { t } = useTranslation();
+  const client = useSelector((state) => state.client.client);
+
+  return (
+    <PageContainer title={t("My Events")!} description={t("My Events")!}>
+      <Breadcrumb
+        title={t("My Events")!}
+        subtitle={t("Manage your events from here.")!}
+        image={client.logo_url}
+      />
+
+      <Grid container spacing={3} justifyContent="center" mt={3} mb={3}>
+        <Grid item xs={12} sm={10} lg={8} textAlign="center">
+          <Typography variant="h2">
+            {t("Here are all your events.")}
+          </Typography>
+          <Typography variant="body1" mt={2}>
+            {t("You can manage your events from here. You can create, edit, delete and view your events. Click on \"Add Event\" to create a new event.")}
+          </Typography>
+        </Grid>
+      </Grid>
+
+      <Grid container justifyContent="right" mt={3} mb={3}>
+        <Grid item>
+          <Button variant="contained" color="primary">
+            {t("Add Event")}
+          </Button>
+        </Grid>
+      </Grid>
+
+      <EventListing />
+    </PageContainer>
+  );
+};
+
+export default Blog;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+use anyhow::{anyhow, Context, Result};
 use clap::Parser;
-use git2::Repository;
-use std::path::Path;
+use fancy_regex::Regex;
+use git2::{Repository, StatusOptions};
+use once_cell::sync::Lazy;
+use serde_json::Value;
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+    path::Path,
+};
 
 #[derive(Parser, Debug)]
 struct Cli {
@@ -8,21 +16,93 @@ struct Cli {
     json_file: String,
 }
 
-fn main() {
+static COMPILED_RE: Lazy<Regex> =
+    Lazy::new(|| Regex::new(r#"(?<![a-zA-Z_])t\(\s*(['"])(.*?)\1\s*\)"#).unwrap());
+
+fn main() -> Result<()> {
     let args = Cli::parse();
-    println!("JSON file: {}", args.json_file);
+    let repo = Repository::open(".").context("Couldn't open the repository")?;
 
-    let repo = Repository::open(".").expect("Couldn't open the repository");
-    let index = repo.index().expect("Couldn't get the index");
+    let mut status_opts = StatusOptions::new();
+    status_opts
+        .include_untracked(true)
+        .recurse_ignored_dirs(false);
+    let statuses = repo
+        .statuses(Some(&mut status_opts))
+        .context("Couldn't get the status of the repository")?;
 
-    // Iterate over the staged files
-    for entry in index.iter() {
-        let path = Path::new(std::str::from_utf8(&entry.path).unwrap());
-        process_file(path);
+    let translations = statuses
+        .iter()
+        .map(|file| process_file(file.path().unwrap()))
+        .collect::<Result<Vec<_>>>()?
+        .into_iter()
+        .flatten()
+        .collect::<HashSet<_>>();
+
+    parse_and_add_translations(&translations, &args.json_file)?;
+    println!("Done adding translations to {}, you should review the newly added translations entries and add the correct values.", args.json_file);
+
+    Ok(())
+}
+
+fn parse_and_add_translations(translations: &HashSet<String>, json_file: &str) -> Result<()> {
+    let json_content = fs::read_to_string(json_file)
+        .with_context(|| format!("Couldn't read the JSON file {}", json_file))?;
+    let mut json: HashMap<String, Value> =
+        serde_json::from_str(&json_content).with_context(|| "Couldn't parse the JSON content")?;
+
+    for translation in translations {
+        json.entry(translation.clone()).or_insert_with(|| {
+            println!("Adding translation: {}", translation);
+            Value::String(String::from("TODO"))
+        });
+    }
+
+    let json_content =
+        serde_json::to_string_pretty(&json).context("Couldn't serialize the JSON")?;
+    fs::write(json_file, json_content)
+        .with_context(|| format!("Couldn't write the JSON file {}", json_file))?;
+
+    Ok(())
+}
+
+fn process_file(file: &str) -> Result<Vec<String>> {
+    let path = Path::new(file);
+    let metadata =
+        fs::metadata(path).with_context(|| format!("Failed to get metadata for {}", file))?;
+    if metadata.is_dir() {
+        let entries =
+            fs::read_dir(path).with_context(|| format!("Couldn't read directory {}", file))?;
+        let mut res = Vec::new();
+        for entry in entries {
+            let entry = entry.with_context(|| "Couldn't get directory entry")?;
+            let path_str = entry
+                .path()
+                .to_str()
+                .ok_or_else(|| anyhow!("Invalid path"))?
+                .to_string();
+            res.extend(process_file(&path_str)?);
+        }
+        Ok(res)
+    } else {
+        let ext = path.extension().and_then(|e| e.to_str()).unwrap_or("");
+        if ["jsx", "tsx", "js", "ts"].contains(&ext) {
+            extract_translations_from_file(&path)
+        } else {
+            Ok(vec![])
+        }
     }
 }
 
-fn process_file(file: &Path) {
-    // Read the file and do something with it
-    println!("Processing file: {:?}", file);
+fn extract_translations_from_file(file: &Path) -> Result<Vec<String>> {
+    let content =
+        fs::read_to_string(file).with_context(|| format!("Couldn't read file {:?}", file))?;
+    let translations = COMPILED_RE
+        .captures_iter(&content)
+        .filter_map(|cap| {
+            cap.ok()
+                .and_then(|c| c.get(2).map(|m| m.as_str().to_string()))
+        })
+        .collect();
+    Ok(translations)
 }
