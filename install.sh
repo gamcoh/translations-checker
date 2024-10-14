59e7bbddca71d0e7f30b85c2d582724734bfddc3
Gamliel Cohen <gamliel.cohen-ext@pernod-ricard.com>
2024-10-15 00:23:34 +0200

    pre-commit way of doing the build

--- install.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-echo "Building the translation checker binary..."
-cargo build --release
--- run_translations_checker.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-# Run the translation_react_checker with the shell args
-./target/release/translation_react_checker "$@"
